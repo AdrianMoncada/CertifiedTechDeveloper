5f5ebffbdc37500bf89728e18eb15a8880c79adf
AdrianMoncada <adrian.ignaciomoncada@gmail.com>
2022-03-04 10:22:59 -0300

    update

---
+++ Front End II/piedraPapelTijeras.js
@@ -0,0 +1,32 @@
+function startGame(){
+  let resultado;
+  let playerSelection = prompt("Piedra, Papel o Tijeras?");
+  if(playerSelection.includes("Piedra")){
+  playerSelection = 3;
+    }else if(playerSelection.includes("Papel")){
+    playerSelection = 2;
+      }else if(playerSelection.includes("Tijeras")){
+      playerSelection = 1;
+      }
+let computerSelection = parseInt(Math.random()*3+1);
+
+if(computerSelection == 3){
+  alert("La computadora seleccionó Piedra");
+} else if(computerSelection == 2){
+  alert("La computadora seleccionó Papel");
+} else{
+  alert("La computadora seleccionó TIjera");
+}
+
+if(playerSelection == 3 && computerSelection == 2 || playerSelection == 2 && computerSelection == 1 || playerSelection == 1 && computerSelection == 3  ){
+  resultado = "Perdiste";
+} else if(playerSelection == 3 && computerSelection == 1 || playerSelection == 2 && computerSelection == 3 || playerSelection == 1 && computerSelection == 2){
+  resultado = "Ganaste";
+} else{
+  resultado = "Empataste";
+}
+
+alert(resultado)
+}
+
+startGame();
