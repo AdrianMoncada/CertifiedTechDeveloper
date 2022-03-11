83b043b77c5f379c9d454c5129d00d346de32a07
AdrianMoncada <adrian.ignaciomoncada@gmail.com>
2022-03-11 11:45:32 -0300

    update

--- OOP/Clase3/src/com/company/Main.java
+++ OOP/Clase3/src/com/company/Main.java
@@ -1,60 +1,63 @@
 package com.company;
-
 import java.util.Scanner;
 
 public class Main {
-    public Main() {
-    }
 
     public static void main(String[] args) {
         jugar();
+
     }
 
-    public static Integer cualGana(String eleccionUno, String eleccionDos) {
+    public static Integer cualGana(String eleccionUno, String eleccionDos){
         String opcionUno = "piedra";
         String opcionDos = "papel";
         String opcionTres = "tijera";
-        if ((!eleccionUno.equals(opcionUno) || !eleccionDos.equals(opcionTres)) && (!eleccionUno.equals(opcionDos) || !eleccionDos.equals(opcionUno)) && (!eleccionUno.equals(opcionTres) || !eleccionDos.equals(opcionDos))) {
-            if ((!eleccionUno.equals(opcionUno) || !eleccionDos.equals(opcionDos)) && (!eleccionUno.equals(opcionDos) || !eleccionDos.equals(opcionTres)) && (!eleccionUno.equals(opcionTres) || !eleccionDos.equals(opcionUno))) {
-                System.out.println("Empate!");
-                return 0;
-            } else {
-                System.out.println("Jugador 2 gana!");
-                return 2;
-            }
-        } else {
+        String opcionCuatro = "spock";
+
+        if(eleccionUno.equals(opcionUno) && eleccionDos.equals(opcionTres) || eleccionUno.equals(opcionDos) && eleccionDos.equals(opcionUno)
+                || eleccionUno.equals(opcionTres) && eleccionDos.equals(opcionDos)) || eleccionUno.equals(opcionCuatro) && eleccionDos.equals(opcionTres)
+                || eleccionUno.equals(opcionCuatro) && eleccionDos.equals(opcionUno){
             System.out.println("Jugador 1 gana!");
             return 1;
+        } else if(eleccionUno.equals(opcionUno) && eleccionDos.equals(opcionDos) || eleccionUno.equals(opcionDos) && eleccionDos.equals(opcionTres)
+                || eleccionUno.equals(opcionTres) && eleccionDos.equals(opcionUno)) || eleccionUno.equals(opcionCuatro) && eleccionDos.equals(opcionDos){
+            System.out.println("Jugador 2 gana!");
+            return 2;
+        } else{
+            System.out.println("Empate!");
+            return 0;
         }
     }
-
-    public static void jugar() {
+    public static void jugar(){
         Scanner scan = new Scanner(System.in);
         System.out.println("Jugador Uno: ");
         String JugadorUno = scan.nextLine();
+
         System.out.println("Jugador Dos: ");
         String JugadorDos = scan.nextLine();
         int contadorUno = 0;
         int contadorDos = 0;
+        while(true){
 
-        while(true) {
-            System.out.println("Elección Jugador Uno: ");
+
+            System.out.println("¿Piedra, papel, tijera o spock?: ");
             String eleccionJugadorUno = scan.nextLine();
-            System.out.println("Elección Jugador Dos: ");
+
+            System.out.println("¿Piedra, papel, tijera o spock?: ");
             String eleccionJugadorDos = scan.nextLine();
-            if (eleccionJugadorUno.contains("*") || eleccionJugadorDos.contains("*")) {
+
+            if(eleccionJugadorUno.contains("*") || eleccionJugadorDos.contains("*")){
                 System.out.println("Uno de los jugadores no quiere jugar más!");
                 System.out.println("Puntuación \n" + JugadorUno + ": " + contadorUno + "\n" + JugadorDos + ": " + contadorDos);
-                return;
+                break;
             }
-
             int ganador = cualGana(eleccionJugadorUno, eleccionJugadorDos);
-            if (ganador == 1) {
+            if(ganador == 1){
                 System.out.println("+1");
-                ++contadorUno;
-            } else if (ganador == 2) {
+                contadorUno++;
+            } else if(ganador == 2){
                 System.out.println("+1");
-                ++contadorDos;
+                contadorDos++;
             }
         }
     }
