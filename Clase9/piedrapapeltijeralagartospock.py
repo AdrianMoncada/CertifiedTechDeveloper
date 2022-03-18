57bcc1b00cf2d20deca5ac2cfef31333b661e05c
AdrianMoncada <adrian.ignaciomoncada@gmail.com>
2022-03-18 17:25:10 -0300

    update

--- Clase9/piedrapapeltijeralagartospock.py
+++ Clase9/piedrapapeltijeralagartospock.py
@@ -2,6 +2,14 @@
 import random
 import sys
 
+# paso1
+
+puntajeUsuario = 0
+puntajePc = 0
+puntajeTotal = 0
+ganaUsuario = 0
+ganaPc = 0
+
 def get_int():
     userdata = input("Ingrese un número, o 's' para salir del programa ")
     if userdata == 's':
@@ -12,7 +20,17 @@ def get_int():
         return user_num
     except ValueError:
         print("Necesito un número para continuar: ")
-        return(get_int())
+        return(get_intget_int())
+
+# paso2
+
+def porcentaje():
+    if puntajeTotal > 0:
+        x = ((puntajeTotal - puntajePc) / puntajeTotal) * 100
+        return x
+    # elif puntajeTotal == 0:
+    #     x = 0
+    #     return x
 
 while True: 
     aleatorio = random.randrange(0, 5)
@@ -23,6 +41,7 @@ while True:
     print("3)Tijera")
     print("4)Lagarto")
     print("5)Spock")
+    print("6)Mostrar Puntajes")
     opcion = get_int()
     
     if opcion == 1:
@@ -35,6 +54,12 @@ while True:
         eligeUsuario = "lagarto"
     elif opcion == 5:
         eligeUsuario = "spock"
+    elif opcion == 6:
+        print ("Puntajes: ")
+        print ("Usuario: ", puntajeUsuario)
+        print ("Pc: ", puntajePc)
+        print ("Porcentaje de victorias: ", porcentaje(), "%")
+        continue
     else:
         print("Opción Invalida")
         continue
@@ -55,47 +80,72 @@ while True:
     
     if eligePc == "piedra" and eligeUsuario == "papel":
         print("Ganaste, papel envuelve piedra")
+        ganaUsuario = 1
     elif eligePc == "papel" and eligeUsuario == "tijera":
         print("Ganaste, tijera corta papel")
+        ganaUsuario = 1
     elif eligePc == "tijera" and eligeUsuario == "piedra":
         print("Ganaste, piedra aplasta tijera")
+        ganaUsuario = 1
     elif eligePc == "lagarto" and eligeUsuario == "piedra":
         print("Ganaste, piedra aplasta a lagarto")
+        ganaUsuario = 1
     elif eligePc == "lagarto" and eligeUsuario == "tijera":
         print("Ganaste, tijera decapita a Lagarto")
+        ganaUsuario = 1
     elif eligePc == "tijera" and eligeUsuario == "spock":
         print("Ganaste, Spock rompe tijera")
+        ganaUsuario = 1
     elif eligePc == "spock" and eligeUsuario == "lagarto":
         print("Ganaste, lagarto envenena a Spock")
+        ganaUsuario = 1
     elif eligePc == "papel" and eligeUsuario == "lagarto":
         print ("Ganaste, lagarto devora papel")
+        ganaUsuario = 1
     elif eligePc == "spock" and eligeUsuario == "papel":
         print ("Ganaste, papel desautoriza a Spock")
+        ganaUsuario = 1
     elif eligePc == "piedra" and eligeUsuario == "spock":
         print ("Ganaste, Spock vaporiza piedra")
+        ganaUsuario = 1
         
     if eligeUsuario == "piedra" and eligePc == "papel":
         print("Perdiste, papel envuelve piedra")
+        ganaPc = 1
     elif eligeUsuario == "papel" and eligePc == "tijera":
         print("Perdiste, tijera corta papel")
+        ganaPc = 1
     elif eligeUsuario == "tijera" and eligePc == "piedra":
         print("Perdiste, piedra aplasta tijera")
+        ganaPc = 1
     elif eligeUsuario == "lagarto" and eligePc == "piedra":
         print("Perdiste, piedra aplasta a lagarto")
+        ganaPc = 1
     elif eligeUsuario == "lagarto" and eligePc == "tijera":
         print("Perdiste, tijera decapita a Lagarto")
+        ganaPc = 1
     elif eligeUsuario == "tijera" and eligePc == "spock":
         print("Perdiste, Spock rompe tijera")
+        ganaPc = 1
     elif eligeUsuario == "spock" and eligePc == "lagarto":
         print("Perdiste, lagarto envenena a Spock")
+        ganaPc = 1
     elif eligeUsuario == "papel" and eligePc == "lagarto":
         print ("Perdiste, lagarto devora papel")
+        ganaPc = 1
     elif eligeUsuario == "spock" and eligePc == "papel":
         print ("Perdiste, papel desautoriza a Spock")
+        ganaPc = 1
     elif eligeUsuario == "piedra" and eligePc == "spock":
         print ("Perdiste, Spock vaporiza piedra")
+        ganaPc = 1
     elif eligePc == eligeUsuario:
         print("Empate")
+        ganaPc = 1
+    
+    puntajeUsuario = puntajeUsuario + ganaUsuario
+    puntajePc = puntajePc + ganaPc
+    puntajeTotal = puntajeUsuario + puntajePc
     while again == "":
         again = input("Jugamos de nuevo? Si/No: ")
         if 'si' in again:
--- Front End II/Clase8/script.js
+++ Front End II/Clase8/script.js
@@ -1,97 +1,73 @@
-window.addEventListener('load', function () {
+window.addEventListener("load", function () {
   //Selecciono los nodos
-  const valor = document.querySelector("#valor")
-  const inicio = document.querySelector("#iniciar")
-  const stop = document.querySelector("#stop")
-  const acelerar = document.querySelector("#acelerar")
-  const pausar = document.querySelector("#pausa")
-  
+  const valor = document.querySelector("#valor");
+  const inicio = document.querySelector("#iniciar");
+  const stop = document.querySelector("#stop");
+  const acelerar = document.querySelector("#acelerar");
+  const pausar = document.querySelector("#pausa");
+
+  let time = 1000
   //inicializo el valor del cronometro
-  let n = 0
+  let n = 0;
 
   function myTimer() {
-  valor.innerText = n++
+    valor.innerText = n++;
   }
-  
 
   let myInterval;
 
-              // TECLADO
-      //setInterval se activa con el evento de teclado
+  //INICIAR
   window.addEventListener("keypress", (event) => {
-      // console.log(event)
-      if(event.key == "Enter") {
-        myInterval = setInterval(myTimer, 1000);
-      }
-  }  )
-
-  window.addEventListener("keypress", (event) => {
-    if(event.keyCode == 32) {
-        clearInterval(myInterval);
-        n = 0
-        valor.innerText = n
+    // console.log(event)
+    if (event.key == "Enter") {
+      clearInterval(myInterval);
+      myInterval = setInterval(myTimer, time);
     }
-  })
+  });
 
+  inicio.addEventListener("click", () => {
+    clearInterval(myInterval);
+    myInterval = setInterval(myTimer, time);
+  });
+
+  //PARAR Y REINICIAR
   window.addEventListener("keypress", (event) => {
-    if(event.key == "a") {
-        clearInterval(myInterval);
+    if (event.keyCode == 32) {
+      clearInterval(myInterval);
+      n = 0;
+      valor.innerText = n;
     }
-  })
-
-  window.addEventListener("keypress", (event) => {
-    if(event.key == "s") {
-        clearInterval(myInterval);
-        myInterval = setInterval(myTimer, 500);
-    }
-  })
-
-  // MOUSE
-
-    inicio.addEventListener("click", () => {
-      
-      myInterval = setInterval(myTimer, 1000);
-  } )
+  });
 
   stop.addEventListener("click", () => {
-      
     clearInterval(myInterval);
-        n = 0
-        valor.innerText = n
-        
-} )
-
-  acelerar.addEventListener("click", () => {
-      
-    clearInterval(myInterval);
-    myInterval = setInterval(myTimer, 500);
-} )
-
-pausar.addEventListener("click", () => {
-      
-  clearInterval(myInterval);
-} )
-
-
-  
-  // stop.addEventListener("mouseover", () => {
-  //     n = 0
-  //     valor.innerText = n
-  // } )
-  
-  //Para que resolvamos ClearInterval:
-  
-  // acelerar.addEventListener("click", () => {
-  //     // clearInterval(intervalo)
-  //     setInterval(() => {
-  //         valor.innerText = n++
-  //      }, 500)
-  // } )
-  
-  
-  
-  
-  
-  
+    n = 0;
+    valor.innerText = n;
   });
-  
+
+  //PAUSAR
+
+  window.addEventListener("keypress", (event) => {
+    if (event.key == "a") {
+      clearInterval(myInterval);
+    }
+  });
+
+  pausar.addEventListener("click", () => {
+    clearInterval(myInterval);
+  });
+});
+
+//ACELERAR
+
+window.addEventListener("keypress", (event) => {
+  if (event.key == "s") {
+    clearInterval(myInterval);
+    myInterval = setInterval(myTimer, time);
+  }
+});
+
+acelerar.addEventListener("click", () => {
+  clearInterval(myInterval);
+  myInterval = setInterval(myTimer, time);
+});
