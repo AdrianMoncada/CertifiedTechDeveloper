2403fe084eb1f8e0eb59f30f29e6987c10b10a72
AdrianMoncada <adrian.ignaciomoncada@gmail.com>
2022-03-25 17:50:40 -0300

    update

--- OOP/C11/src/com/company/Animal.java
+++ OOP/C11/src/com/company/Animal.java
@@ -1,4 +1,13 @@
 package com.company;
 
-public class Animal {
+public abstract class Animal {
+    private String nombre;
+    private Integer edad;
+
+    public Animal(String nombre, Integer edad){
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public abstract void hacerRuido();
 }
--- OOP/C11/src/com/company/Main.java
+++ OOP/C11/src/com/company/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Animal animal = new Perro("Kimba",3);
+        animal.hacerRuido();
     }
 }
