619c21b8387c8a82b9d498d7ae4a84a28163952d
AdrianMoncada <adrian.ignaciomoncada@gmail.com>
2022-03-25 17:41:44 -0300

    update

---
+++ FEND-II/C12/script.js
@@ -0,0 +1,66 @@
+const nombreUsuario = document.querySelector("#nombre")
+const contraseniaUsuario = document.querySelector("#pass")
+const emailUsuario = document.querySelector("#email")
+const formulario = document.querySelector("form")
+
+
+let usuario = {
+    nombreCompleto: "",
+    contra: "",
+    email: "",
+}
+
+
+let span = document.createElement("span")
+let nameError = document.querySelector("#nameError")
+
+
+
+function normalizarDatos(nombre) {
+    let n = nombre.trim().toUpperCase()
+    if(n.length <= 50) {
+        usuario.nombreCompleto = n
+    }
+    else {
+        nombreUsuario.classList.add("error")
+        nameError.appendChild(span)
+        span.innerText = "Se excedio de la cantidad permitida de caracteres"
+
+    }
+}
+
+function validaremail(email) {
+        usuario.email = parseInt(email) 
+}
+
+nombreUsuario.addEventListener("blur", () => {
+    normalizarDatos(nombreUsuario.value)
+})
+
+contraseniaUsuario.addEventListener("change", () => {
+    let strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,16})");
+
+    if (strongRegex.test(contraseniaUsuario.value)) {
+        usuario.contrasenia = contraseniaUsuario.value
+    }
+    else {
+        alert("contra invalida")
+    }
+    console.log(usuario)
+   })
+
+formulario.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    validarEmail(emailUsuario.value)
+
+    checkbox.forEach( hobbie => {
+    hobbie.checked ? usuario.hobbiesSeleccionados.push(hobbie.id) : null
+    })
+
+    radio.forEach( nac => {
+    nac.checked ? usuario.nacionalidad = nac.id : null
+    })
+
+    console.log(usuario)
+})
