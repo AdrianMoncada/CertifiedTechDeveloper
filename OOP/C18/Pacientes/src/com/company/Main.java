87921160d8eb5ce69af8da596062d1131a0ff63f
AdrianMoncada <adrian.ignaciomoncada@gmail.com>
2022-04-09 19:47:46 -0300

    update

--- OOP/C18/Pacientes/src/com/company/Main.java
+++ OOP/C18/Pacientes/src/com/company/Main.java
@@ -4,7 +4,6 @@ import java.util.Date;
 public class Main {
 
     public static void main(String[] args) {
-
         try {
             Paciente paciente= new Paciente("Juan","Perez","12345",new Date("04/09/2022 19:35:00"));
             paciente.getFechaInternacion();
