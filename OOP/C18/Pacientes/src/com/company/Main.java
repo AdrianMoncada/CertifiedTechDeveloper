4184632ea7cbaed42ed5b30e2dcb53f7dfa859fb
AdrianMoncada <adrian.ignaciomoncada@gmail.com>
2022-04-09 19:33:33 -0300

    update

--- OOP/C18/Pacientes/src/com/company/Main.java
+++ OOP/C18/Pacientes/src/com/company/Main.java
@@ -1,15 +1,17 @@
 package com.company;
 
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Main {
 
     public static void main(String[] args) {
+
         try {
-            Paciente paciente= new Paciente("Juan","Perez","12345",new Date("04/10/2022"));
+            Paciente paciente= new Paciente("Juan","Perez","12345",new Date("04/09/2022 19:35:00"));
             paciente.getFechaInternacion();
-            paciente.darAlta(new Date("04/11/2022"));
+            paciente.darAlta(new Date("04/11/2022 12:25:56"));
             paciente.getFechaAlta();
         } catch (PacienteException e) {
             e.printStackTrace();
