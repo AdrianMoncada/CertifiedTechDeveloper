652823550f509507dedd63ab9e5971c634773b8e
AdrianMoncada <adrian.ignaciomoncada@gmail.com>
2022-05-28 17:39:00 -0300

    update

--- Backend/C12/empleados/src/Main.java
+++ Backend/C12/empleados/src/Main.java
@@ -1,16 +1,17 @@
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
 /*UPDATE EMPLEADO
-        SET COUNTRY = 'Brasil'
-        WHERE COUNTRY='Argentina';*/
-
-/*UPDATE EMPLEADO
         SET COUNTRY = 'Alemania'
         WHERE ID=1;*/
 public class Main {
+    private final static Logger logger = Logger.getLogger(Main.class);
     private static final String SQL_CREATE_TABLE = "DROP TABLE IF EXISTS EMPLEADO; CREATE TABLE EMPLEADO "
             + "("
             + " ID INT PRIMARY KEY,"
@@ -23,6 +24,12 @@ public class Main {
     private static final String SQL_INSERT = "INSERT INTO EMPLEADO (ID, EMAIL, NAME, COUNTRY, LASTNAME) VALUES (1,'federicodiaz@gmail.com','Federico','Argentina','Diaz')";
     private static final String SQL_INSERT2 = "INSERT INTO EMPLEADO (ID, EMAIL, NAME, COUNTRY, LASTNAME) VALUES (2,'agustinapodesta@gmail.com','Agustina','Brasil','Podesta')";
     private static final String SQL_INSERT3 = "INSERT INTO EMPLEADO (ID, EMAIL, NAME, COUNTRY, LASTNAME) VALUES (3,'pedroalonso@gmail.com','Pedro','Argentina','Alonso')";
+    private static final String SQL_INSERT4 = "UPDATE EMPLEADO\n" +
+            "SET COUNTRY = 'Argelia'\n" +
+            "WHERE ID=3;";
+
+    private static final String SQL_INSERT5 = "DELETE FROM EMPLEADO\n" +
+            "WHERE ID=1;";
 
     public static Connection getConnection() throws Exception {
         Class.forName("org.h2.Driver").newInstance();
@@ -32,6 +39,8 @@ public class Main {
 
 
     public static void main(String[] args) throws Exception {
+        File log4jfile = new File("./src/logging/config/log4j.properties");
+        PropertyConfigurator.configure(log4jfile.getAbsolutePath());
         Connection connection = null;
 
         try{
@@ -42,18 +51,30 @@ public class Main {
             statement.execute(SQL_INSERT);
             statement.execute(SQL_INSERT2);
             statement.execute(SQL_INSERT3);
+            statement.execute(SQL_INSERT4); /*actualizar a uno de los empleados con un nuevo valor*/
+            statement.execute(SQL_INSERT5);
+
 
             String sql = "SELECT * FROM EMPLEADO\n" +
-                    "WHERE COUNTRY = 'Argentina';";
+                    "WHERE ID = 1;\n";
 
             ResultSet rd = statement.executeQuery(sql);
 
             while (rd.next()) {
-                System.out.println(rd.getInt(1) + rd.getString(2) + rd.getString(3));
+                logger.debug("ID: " + rd.getInt(1) + " MAIL: " + rd.getString(2) +
+                        " NAME: " + rd.getString(3) + " COUNTRY: " + rd.getString(4) +
+                        " LASTNAME: " + rd.getString(5));
             }
 
+            /*String sql2 = "SELECT * FROM EMPLEADO\n" +
+                    "WHERE ID = 3;\n";
+
+
+             //*eliminar un empleado según el ID*/
+
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("error: ", e);
+            /*e.printStackTrace();*/
         } finally {
             connection.close();
         }
