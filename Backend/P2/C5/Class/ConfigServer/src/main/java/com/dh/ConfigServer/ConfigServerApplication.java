305db3bd4bc627970b01c3ae48a1ea5cae155351
Adrián Moncada <adrian.ignaciomoncada@gmail.com>
2022-10-27 21:02:45 -0300

    update

--- Backend/P2/C5/Class/ConfigServer/src/main/java/com/dh/ConfigServer/ConfigServerApplication.java
+++ Backend/P2/C5/Class/ConfigServer/src/main/java/com/dh/ConfigServer/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.dh.configserver;
+package com.dh.ConfigServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ Backend/P2/C5/dh-cuenta-service/src/test/java/com/dh/consumerservice/DhCuentaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dh.consumerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhCuentaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
